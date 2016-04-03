7fc5c97a3bcb7797ab99458cdaa39e16004c42a8
stanko75 <Genije123>
2016-04-03 13:25:40 +0200

    On mouse enter I am stopping the animation, on mouse leave I am starting the animation again

--- js/joomla.slider.js
+++ js/joomla.slider.js
@@ -1,25 +1,45 @@
-/*global jQuery, document, setInterval, window*/
+/*global jQuery, document, setInterval, window, clearInterval*/
 (function (ns) {
     "use strict";
     var readEXIFdataCallback,
-        googleMapsCallback;
+        googleMapsCallback,
+        intervalHandler;
 
     readEXIFdataCallback = new ns.ReadEXIFdata();
 
     googleMapsCallback = new ns.GoogleMaps();
 
+    function animateImagesTrack() {
+        var clsMilosev = jQuery(".milosev");
+        jQuery("#milosevImagesTrack").append('<img src="' + clsMilosev[0].src + '" class="milosev">');
+        clsMilosev.eq(0).remove();
+        clsMilosev.eq(2).css("border", "14px solid #333");
+        clsMilosev.eq(1).css("border", "");
+        ns.Visualize(clsMilosev.eq(2)[0].src, readEXIFdataCallback.readImage, googleMapsCallback.setMarker);
+    }
+
+    function intervalManager(doAnimate) {
+        if (doAnimate) {
+            intervalHandler = setInterval(animateImagesTrack, 500);
+        } else {
+            clearInterval(intervalHandler);
+        }
+    }
+
     jQuery.getJSON("js/files.json", function (data) {
         data.forEach(function (file) {
             jQuery("#milosevImagesTrack").append('<img src="' + file + '" class="milosev">');
         });
-        setInterval(function () {
-            var clsMilosev = jQuery(".milosev");
-            jQuery("#milosevImagesTrack").append('<img src="' + clsMilosev[0].src + '" class="milosev">');
-            clsMilosev.eq(0).remove();
-            clsMilosev.eq(2).css("border", "14px solid #333");
-            clsMilosev.eq(1).css("border", "");
-            ns.Visualize(clsMilosev.eq(2)[0].src, readEXIFdataCallback.readImage, googleMapsCallback.setMarker);
-        }, 500);
+        intervalManager(true);
     });
+
+    jQuery("#milosevImagesTrack").mouseenter(function () {
+        intervalManager(false);
+    });
+
+    jQuery("#milosevImagesTrack").mouseleave(function () {
+        intervalManager(true);
+    });
+
 }(window.joomla));
 //@ sourceURL=joomla.slider.js
